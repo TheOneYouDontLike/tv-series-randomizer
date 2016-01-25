9ed1386ae8fd8fb40c19a6703d9ce666621af68e
kjendrzyca <kjendrzyca@gmail.com>
2016-01-25 23:00:15 +0100

    hello world

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './root'
 
-const root = document.getElementById('main-container')
+const rootElement = document.getElementById('main-container')
 
-ReactDOM.render(<Root />, root)
+ReactDOM.render(<Root />, rootElement)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express'),
+      path    = require('path'),
+      logger  = require('./logger')
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const PUBLIC_PATH = path.resolve(__dirname, 'public')
 
-app.listen(PORT)
+app.use('/public', express.static(PUBLIC_PATH))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  logger.log('info', 'listening on port ', PORT)
+})
